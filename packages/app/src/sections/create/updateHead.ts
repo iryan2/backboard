b96c3b0a58e567e1e09a01b96bb7fcd66c0170e9
Ian Ryan <iryan2@gmail.com>
2022-04-26 20:21:28 +1000

    Fix head updates

--- packages/app/src/sections/create/updateHead.ts
+++ packages/app/src/sections/create/updateHead.ts
@@ -1,8 +1,17 @@
 import { getGithubInstance } from 'features/auth/getGithubInstance'
 import type { Head, NewLink } from './postNewLink'
 
-export async function updateHead(newLink: NewLink, head: Head) {
+export async function updateHead(newLink: NewLink) {
   const github = getGithubInstance()
+
+  // update head pointer
+  const { data: headData } = await github.get('head.json')
+  const head = {
+    content: JSON.parse(atob(headData.content)),
+    sha: headData.sha,
+  }
+  head.content.head = newLink.id
+  console.log('head content', head.content)
 
   const updatedHeadResponse = await github.put('head.json', {
     message: `Update head for link: ${newLink.title}\n\n${newLink.href}`,
